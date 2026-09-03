5cc703cdce234b28138da4bad5a8ea7d07b43e77
vardhancodes <vardhanviraj05@gmail.com>
2026-09-03 12:11:01 +0530

    Added new stream

--- AdditionalJava.java
+++ AdditionalJava.java
@@ -1,11 +1,17 @@
 import java.util.*;
 import java.util.stream.Stream;
 
+//STREAM API
 public class AdditionalJava {
     public static void main(String[] args) {
         List<Integer> nums = Arrays.asList(5,4,6,8,2,3);
 
         Stream<Integer> newStream = nums.stream();
         newStream.forEach((Integer n) -> System.out.println(n));
+
+        Stream<Integer> sortedStream = nums.stream().sorted();
+        sortedStream.forEach((Integer n) -> System.out.println(n));
+
+
     }
 }
