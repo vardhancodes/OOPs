d4898503732d74e4464e43f0445a099f4a096a1e
vardhancodes <vardhanviraj05@gmail.com>
2026-09-06 00:47:41 +0530

    Runtime Error Example

--- LaunchEH1.java
+++ LaunchEH1.java
@@ -1,5 +1,14 @@
+//Runtime Error
+class Telusko{
+    
+    public void courses()
+    {
+        courses();
+    }
+}
 public class LaunchEH1 {
     public static void main(String[] args) {
-            
+        Telusko t = new Telusko();
+        t.courses();
     }
 }
