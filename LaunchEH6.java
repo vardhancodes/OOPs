c11e5b8f888bde88f16230f7f179076d32347f4e
vardhancodes <vardhanviraj05@gmail.com>
2026-09-19 17:18:48 +0530

    learnt throws

--- LaunchEH6.java
+++ LaunchEH6.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Alpha
 {
-    public void divide()
+    public void divide() throws ArithmeticException
     {
         try {
             Scanner sc = new Scanner(System.in);
@@ -9,13 +9,23 @@ class Alpha
             int n = sc.nextInt();
             System.out.println("Kindly! Enter denominator");
             int d = sc.nextInt();
+            if(d < 0)
+            {
+                throw new ArithmeticException();
+            }
             int res = n/d;
             System.out.println(res);
             
         }catch (Exception e) {
             System.out.println("Exception Handled in alpha");
             throw e;
+            
         }
+        finally{
+            System.out.println("App terminated");
+
+        }
+
         
     }
     
@@ -27,6 +37,14 @@ public class LaunchEH6 {
     public static void main(String[] args)
     {
         Alpha a = new Alpha();
+        try{
         a.divide();
+        }
+
+        catch(ArithmeticException e)
+        {
+            System.out.println("Handled in main");
+
+        }
     }
 }
